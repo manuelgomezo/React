af5e9927532964010a7df813ba4a672f37aca7d4
jersonlopez <jersonlopez.1204@gmail.com>
2017-04-08 11:32:45 -0500

    Primera version Dojo 3

---
+++ src/SearchBar.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.handleFilterTextInputChange = this.handleFilterTextInputChange.bind(this);
+    this.handleInStockInputChange = this.handleInStockInputChange.bind(this);
+  }
+  
+  handleFilterTextInputChange(e) {
+    this.props.onFilterTextInput(e.target.value);
+  }
+  
+  handleInStockInputChange(e) {
+    this.props.onInStockInput(e.target.checked);
+  }
+  
+  render() {
+    return (
+      <form>
+        <input type="text" value={this.props.filterText} onChange={this.handleFilterTextInputChange}/>
+        <p>
+          <input type="checkbox" checked={this.props.inStockOnly} onChange={this.handleInStockInputChange} />
+        </p>
+      </form>
+    );
+  }
+}
